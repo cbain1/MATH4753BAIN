9e0de112efcae5d89c90772dac6eb4fd299498ef
Catherine Bain <cbain1@butler.edu>
2021-03-19 10:33:54 -0500

    fire data documentation

---
+++ R/fire.R
@@ -0,0 +1,11 @@
+#' FIREDAM data set
+#'
+#' A data set collected containing information on how far away a particular fire was from a fire station and the cooresponding amount of fire damage the fire caused
+#'
+#' @format A data frame with 15 rows and 2 variables (columns):
+#' \describe{
+#' \item{DISTANCE}{distance from fire station, in miles}
+#' \item{DAMAGE}{damage caused by the fire, in thousands of dollars}
+#' }
+#' @source \url{https://www.crcpress.com/Statistics-for-Engineering-and-the-Sciences/Mendenhall-Sincich/p/book/9781498728850}
+#' "fire"
